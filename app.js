78f6562cad9961abd2afd00a62f5c73c3220afce
Chev-v <20xavier.almeida05@gmail.com>
2025-02-21 00:24:18 -0500

    Final Portfolio ready for cloud upload
    Everything is fully completed, small details like error handling, links and mail to features were added. I have fully filled every page with information on me as well as things I enjoy doing. Another things that Im proud of and looks nice is the Projects page.

--- app.js
+++ app.js
@@ -4,28 +4,39 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import the router for the index route
 var indexRouter = require('./routes/index');
+// Import the router for the users route
 var usersRouter = require('./routes/users');
 
+// Init the express app
 var app = express();
 
-// view engine setup
+// Set up the view using the express-handlebars
 const exphbs = require('express-handlebars');
-
+// Setup Handlebars to use .hbs as the file ext for views
 app.engine('hbs', exphbs.engine({ extname: 'hbs' }));
+// Set the view engine to hbs
 app.set('view engine', 'hbs');
 
-
+// Use the logger middleware in dev mode
 app.use(logger('dev'));
+// Parse JSON reqs
 app.use(express.json());
+// Parse URL-encoded payloads
 app.use(express.urlencoded({ extended: false }));
+// Parse cookies attached to the client request object
 app.use(cookieParser());
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+// Define the route for '/users' using the users router
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 errors and forward them to the error handler, 
+// Also this stopped my site from crashing when using random website directories like /random or just a misspelled one.
+// I found this and encorperated it mainly because I actually want to use this portfolio for potential jobs or even just CO-OPs, and these small details matter. 
 app.use(function(req, res, next) {
   res.status(404).render('error', {
     title: '404 - Page Not Found',
@@ -34,15 +45,16 @@ app.use(function(req, res, next) {
   });
 });
 
-// error handler
+// Error handler middleware
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set local variables; provide error details only in dev mode
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page with the appropriate  code
   res.status(err.status || 500);
   res.render('error');
 });
 
+// Export the app module for use in other parts of the app
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+// Home route it renders the home page
 router.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
+// About route, it renders the about page
 router.get('/about', (req, res) => {
     res.render('about', { title: 'About Me' });
 });
 
+// Projects route it renders the projects page
 router.get('/projects', (req, res) => {
     res.render('projects', { title: 'Projects' });
 });
 
+// Contact route it renders the contact page
 router.get('/contact', (req, res) => {
     res.render('contact', { title: 'Contact' });
 });
 
+// Export the router to be used in app.js
 module.exports = router;
